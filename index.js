779b6d684e6a98c551b17f0bade5236d4034d978
eli <eblock@gmail.com>
2014-11-12 03:13:55 -0800

    Add basic IRC support, command parsing, and some commands (@weather, @help, and @eval)

---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var irc = require('irc');
+var util = require('util');
+
+var commandMap = {};
+
+// TODO: Implement proper config/settings
+var config = require("./NiceBot.json");
+
+// node-irc api docs @ https://node-irc.readthedocs.org/en/latest/API.html
+var client = new irc.Client(config.irc.serverHost, config.irc.nickName, config.irc);
+
+// implement dynamic command registration
+commandMap["eval"] = require("./lib/eval.js"); // jshint ignore:line
+commandMap["weather"] = require("./lib/weather.js");
+
+commandMap["help"] = function (client, from, channel, command, args) {
+    var commands = Object.getOwnPropertyNames(commandMap);
+    client.say(channel, "Available commands: " + commands.join(', '));
+};
+
+client.on('error', function (err) {
+});
+
+client.on('message', function (from, to, message) {
+    if (message) {
+        var command = matchGroup(message, /^@(\w+)/, 1 /*groupIndex*/);
+        if (command) {
+            command = command.toLowerCase();
+            var args = matchGroup(message, /\s+(.+)/, 1 /*groupIndex*/);
+            if (commandMap[command]) {
+                commandMap[command](client, from, to, command, args);
+            }
+        }
+    }
+});
+
+client.on('join', function (channel, nick, message) {
+});
+
+client.connect();
+
+function matchGroup(str, regex, groupIndex) {
+    var result;
+    var matches = str.match(regex);
+    if (matches && (matches.length > groupIndex)) {
+        result = matches[groupIndex];
+    }
+    return result;
+}
